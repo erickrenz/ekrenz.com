ed3542058a6c715a7b8aa403d084ccb4107ac9d4
krenzew <54631709+krenzew@users.noreply.github.com>
2023-02-17 13:50:37 +0100

    remove partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-import partytown from "@astrojs/partytown";
 
 export default defineConfig({
-  integrations: [tailwind(), partytown()]
+  integrations: [tailwind()]
 });
