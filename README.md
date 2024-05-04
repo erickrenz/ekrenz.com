e5ab73c5c269ca34f91c6ca31f7a68b58b547468
erickrenz <krenz.eric.krenz@gmail.com>
2024-05-04 12:03:51 +0200

    feat: fresh start

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # [ekrenz.com](https://ekrenz.com)
-
-My personal website was created using [Astro](https://astro.build) and [Tailwind CSS](https://tailwindcss.com).
--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-
-export default defineConfig({
-  integrations: [tailwind()]
-});
