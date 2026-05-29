40468a989f2513b30dc5361f56e0a43ba74931a2
Eric Krenz <krenz.eric.krenz@gmail.com>
2026-05-29 12:00:34 -0400

    Update Astro and Tailwind dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [icon()]
 });
